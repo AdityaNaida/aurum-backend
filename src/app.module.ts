fbccc83cf0db576bfa14988f8dc017d703aa3875
Aditya Naida <adityanaida@gmail.com>
2025-03-29 01:07:20 +0530

    [ADD] Rediss

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import * as redisStore from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    // Load environment variables
+    ConfigModule.forRoot(),
+
+    // Configure Redis caching
+    CacheModule.register({
+      store: redisStore as any, // Fix TypeScript issue
+      url: process.env.REDIS_URL, // Redis connection URL
+      isGlobal: true,
+      ttl: 60, // Time to live for cache (in seconds)
+      max: 100, // Max number of items in cache
+    }),
+
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      ttl: 5, // Default cache time-to-live (seconds)
+      max: 100, // Maximum number of cached items
+    }),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
 })
